1eae70374cac81fa5d4aa8d63e0a8bd21e5ef4f5
nikitadada <nikitadada@gmail.com>
2018-11-13 20:28:54 +0300

    added row iterator

--- src/Command/Excel/ReadFileCommand.php
+++ src/Command/Excel/ReadFileCommand.php
@@ -11,19 +11,36 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ReadFileCommand extends BaseCommand
 {
+    private $reader;
+
+    public function __construct(Reader $reader)
+    {
+        $this->reader = $reader;
+
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
-            ->addArgument('file-name', InputArgument::REQUIRED, 'File name')
-        ;
+            ->addArgument('file-name', InputArgument::REQUIRED, 'File name');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $fileName = $input->getArgument('file-name');
 
-        $reader = new Reader($fileName);
-        $spreadsheet = $reader->getSpreadsheet();
+        $spreadsheet = $this->reader->getSpreadsheet($fileName);
+        $worksheet = $spreadsheet->getActiveSheet();
+
+        foreach ($worksheet->getRowIterator() as $row) {
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(FALSE);
+            foreach ($cellIterator as $cell) {
+                var_dump($cell->getValue());
+
+            }
+        }
 
         $fileName = $input->getArgument('file-name');
         $output->writeln("<info>file name: $fileName</info>");
